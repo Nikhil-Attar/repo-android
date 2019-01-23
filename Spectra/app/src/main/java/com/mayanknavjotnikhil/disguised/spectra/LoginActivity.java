25a8f4de503bc16f407e030881c470539c5585c9
Mayank Kumar <mayankkumar7668@gmail.com>
2019-01-23 22:56:54 +0530

    15, 16 : added firebase authentications, resources and new layouts

---
+++ Spectra/app/src/main/java/com/mayanknavjotnikhil/disguised/spectra/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.mayanknavjotnikhil.disguised.spectra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login_activity);
+    }
+}
---
+++ Spectra/app/src/main/java/com/mayanknavjotnikhil/disguised/spectra/Profile.java
@@ -0,0 +1,12 @@
+package com.mayanknavjotnikhil.disguised.spectra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.profile_layout);
+    }
+}
